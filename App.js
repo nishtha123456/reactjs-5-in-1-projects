7ccd0fb11929b5c8f084affa02f1e3ea1f40757d
nishtha123456 <108168210+nishtha123456@users.noreply.github.com>
2022-07-22 11:09:55 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,26 +1,69 @@
 //import logo from './logo.svg';
-
+import React from 'react';
 import './App.css';
-import {BrowserRouter as Router } from "react-router-dom";
+import {BrowserRouter as Router, Routes,Route} from "react-router-dom";
 import Navbar from './Components/Navbar';
 import Main from './Components/Main';
+import Signup from './Components/Signup'
 import Footer from './Components/Footer';
-//import About from './Components/About';
-//import Footer from './component/Footer';
-//import Textform from './component/Textform';
-//import Home from './Components/Home';
-//import About from './Components/About';
-//import Textutill from './Components/Textutill';
-//import Timer from './Components/Timer';
-//import Todolist from './Components/Todolist';
-//import Counter from './Components/Counter';
+import Login from './Components/Login'
 
-function App() {
+import Home from './Components/Home';
+import About from './Components/About';
+import Timer from './Components/Timer';
+import Textutill from './Components/Textutill';
+import Counter from './Components/Counter'
+import Todolist from './Components/Todolist'
+import ReadFile from './Components/ReadFile'
+import Increment from './Components/Increment'
+import ReduxTodolist from './Components/ReduxTodolist'
+//import {auth} from './Components/Login';
+//import reportWebvitals from './reportWebVitals'
+//import Store from './Components/Store';
+//import {Provider} from 'react-redux';
+function App () {
+ // constructor(props){
+   // super(props);
+   // auth=0;
+  //}
+
   return (
   <Router>
-    <div><Navbar name='Home'/></div>
-    <div><Main /></div>
-   <div><Footer/></div>
+    <div><Navbar name='Home'  /></div>
+
+  <div >
+    <Routes>
+    <Route path="/" element={<Signup/>} />
+    <Route path="/Home" element={<Home/>} />
+    <Route path="/About" element={<About/>} />
+    <Route path="/Signup" element={<Signup/>} />
+    <Route path="/Login"  element={<Login />} />
+  
+  
+    <Route path="/Login/Main" element={<Main/>} >
+
+    
+    <Route   path="/Login/Main/Timer" element={<Timer/>} />
+    <Route   path="/Login/Main/Textutill" element={<Textutill/>} />
+    <Route   path="/Login/Main/Counter" element={<Counter/>} />
+    <Route   path="/Login/Main/Todolist" element={<Todolist/>} />
+    <Route   path="/Login/Main/ReadFile" element={<ReadFile/>} />
+    <Route   path="/Login/Main/Increment" element={<Increment/>} />
+    <Route   path="/Login/Main/ReduxTodolist" element={<ReduxTodolist/>} />
+    <Route   path="./About" element={<About/>} />
+      </Route>
+  
+    
+
+    </Routes>
+  
+
+    
+    
+    
+    </div>
+      
+     <div><Footer/></div>
 </Router>         
   );
 }
