2ba49294a00c1484e5c0d5afd6ff23e2f7bc8090
nishtha123456 <108168210+nishtha123456@users.noreply.github.com>
2022-11-26 10:47:15 +0530

    Update asha_node2.js

--- asha_node2.js
+++ asha_node2.js
@@ -1,7 +1,7 @@
 
 //BACK END FILE
 
-   const express = require('express');
+  const express = require('express');
   // const request=require('request');
 
    const app = express()
@@ -10,21 +10,28 @@
    cors = require("cors");
 
    app.use(cors());
-
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
    app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
 
    
-   app.get('/', (req, res) => {
+   app.get('/', async(req, res) => {
     const customers=[
-      {id:1 , firstname:"asha",  lastname:"savani"},
-      {id:2 , firstname:"nishtha",  lastname:"savani"},
-      {id:3 , firstname:"yaksh",  lastname:"savani"}
+{id:1 , firstname:"asha",  lastname:"savani",img:"https://www.hilanddairy.com/image-library/sites/default/files/Hiland_ToGo_OJ_16oz.jpg"},
+      {id:2 , firstname:"nishtha",  lastname:"savani",img:"C:\Users\pc4\Documents\INFOVIRTECH-TASK\Restaurant-meu\restaurant.png"},
+      {id:3 , firstname:"yaksh",  lastname:"savani",img:"C:\Users\pc4\Documents\INFOVIRTECH-TASK\Restaurant-meu\restaurant.png"}
    
      ]
    res.json({customers})  ;
+   })
+   app.post('/',async(req,res)=>{
+   let {name}=req.body
+   console.log(name)
 
+   })
+  
  // var id=2;
    //  res.send({id})
    })
